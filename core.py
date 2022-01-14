8d1a8e3108d04745c765780dd573017dc2a7f74e
Michael Hippke <hippke@users.noreply.github.com>
2022-01-14 12:44:06 +0100

    Add files via upload

--- core.py
+++ core.py
@@ -404,6 +404,156 @@ def occult(zs, k, u1, u2):
 
 
 @jit(nopython=True, fastmath=True)
+def occult_classic(zs, k, u1, u2):
+    """Evaluates the transit model for an array of normalized distances.
+    Parameters
+    ----------
+    z: 1D array
+        Normalized distances
+    k: float
+        Planet-star radius ratio
+    u1, u2: float
+        Limb darkening coefficients
+    Returns
+    -------
+    Transit model evaluated at `z`.
+    """
+    if abs(k - 0.5) < 1e-4:
+        k = 0.5
+
+    INV_PI = 1 / pi
+    k2 = k ** 2
+    lzs = len(zs)
+    flux = np.empty(lzs)
+    le = np.empty(lzs)
+    ld = np.empty(lzs)
+    ed = np.empty(lzs)
+    omega = 1 / (1 - u1 / 3 - u2 / 6)
+    c1 = 1 - u1 - 2 * u2
+    c2 = u1 + 2 * u2
+
+    for i in range(lzs):
+        z = zs[i]
+
+        if abs(z - k) < 1e-6:
+            z += 1e-6
+
+        # The source is unocculted
+        if z > 1 + k or z < 0:
+            flux[i] = 1
+            le[i] = 0
+            ld[i] = 0
+            ed[i] = 0
+            continue
+
+        # The source is completely occulted
+        elif k >= 1 and z <= k - 1:
+            flux[i] = 0
+            le[i] = 1
+            ld[i] = 1
+            ed[i] = 1
+            continue
+
+        z2 = z ** 2
+        x1 = (k - z) ** 2
+        x2 = (k + z) ** 2
+        x3 = k ** 2 - z2
+
+        # Eq. 26: Star partially occulted and the occulting object crosses the limb
+        if z >= abs(1 - k) and z <= 1 + k:
+            kap1 = arccos(min((1 - k2 + z2) / (2 * z), 1))
+            kap0 = arccos(min((k2 + z2 - 1) / (2 * k * z), 1))
+            le[i] = k2 * kap0 + kap1
+            le[i] = (le[i] - 0.5 * sqrt(max(4 * z2 - (1 + z2 - k2) ** 2, 0))) * INV_PI
+
+        # Occulting object transits the source star (but doesn't completely cover it):
+        if z <= 1 - k:
+            le[i] = k2
+
+        # Edge of occulting body lies at the origin: special expressions in this case:
+        if abs(z - k) < 1e-4 * (z + k):
+            # ! Table 3, Case V.:
+            if k == 0.5:
+                ld[i] = 1 / 3 - 4 * INV_PI / 9
+                ed[i] = 3 / 32
+            elif z > 0.5:
+                ld[i] = (
+                    1 / 3
+                    + 16 * k / 9 * INV_PI * (2 * k2 - 1) * ellec(0.5 / k)
+                    - (32 * k ** 4 - 20 * k2 + 3) / 9 * INV_PI / k * ellk(0.5 / k)
+                )
+                dist = sqrt((1 - x1) * (x2 - 1))
+                ed[i] = (
+                    1
+                    / 2
+                    * INV_PI
+                    * (kap1 + k2 * (k2 + 2 * z2) * kap0 - (1 + 5 * k2 + z2) / 4 * dist)
+                )
+            elif z < 0.5:
+                # ! Table 3, Case VI.:
+                ld[i] = 1 / 3 + 2 / 9 * INV_PI * (
+                    4 * (2 * k2 - 1) * ellec(2 * k) + (1 - 4 * k2) * ellk(2 * k)
+                )
+                ed[i] = k2 / 2 * (k2 + 2 * z2)
+
+        # Occulting body partly occults the source and crosses the limb:
+        # Table 3, Case III:
+        if (z > 0.5 + abs(k - 0.5) and z < 1 + k) or (
+            k > 0.5 and z > abs(1 - k) and z < k
+        ):
+            q = sqrt((1 - (k - z) ** 2) / 4 / z / k)
+            ld[i] = (
+                1
+                / 9
+                * INV_PI
+                / sqrt(k * z)
+                * (
+                    ((1 - x2) * (2 * x2 + x1 - 3) - 3 * x3 * (x2 - 2)) * ellk(q)
+                    + 4 * k * z * (z2 + 7 * k2 - 4) * ellec(q)
+                    - 3 * x3 / x1 * ellpicb((1 / x1 - 1), q)
+                )
+            )
+            if z < k:
+                ld[i] = ld[i] + 2 / 3
+            ed[i] = (
+                1
+                / 2
+                * INV_PI
+                * (
+                    kap1
+                    + k2 * (k2 + 2 * z2) * kap0
+                    - (1 + 5 * k2 + z2) / 4 * sqrt((1 - x1) * (x2 - 1))
+                )
+            )
+
+        # Occulting body transits the source:
+        # Table 3, Case IV.:
+        if k <= 1 and z < (1 - k):
+            q = sqrt((x2 - x1) / (1 - x1))  # recalc because different condition
+            ld[i] = (
+                2
+                / 9
+                * INV_PI
+                / sqrt(1 - x1)
+                * (
+                    (1 - 5 * z2 + k2 + x3 * x3) * ellk(q)
+                    + (1 - x1) * (z2 + 7 * k2 - 4) * ellec(q)
+                    - 3 * x3 / x1 * ellpicb((x2 / x1 - 1), q)
+                )
+            )
+            if z < k:
+                ld[i] = ld[i] + 2 / 3
+            if abs(k + z - 1) < 1e-4:
+                ld[i] = 2 / 3 * INV_PI * arccos(1 - 2 * k) - 4 / 9 * INV_PI * sqrt(
+                    k * (1 - k)
+                ) * (3 + 2 * k - 8 * k2)
+            ed[i] = k2 / 2 * (k2 + 2 * z2)
+
+        flux[i] = 1 - (c1 * le[i] + c2 * ld[i] + u2 * ed[i]) * omega
+    return flux
+
+
+@jit(nopython=True, fastmath=True)
 def occult_hybrid(zs, k, u1, u2):
     """Evaluates the transit model for an array of normalized distances.
     Parameters
@@ -855,6 +1005,7 @@ def x_bary_grid(time, a_bary, per_bary, t0_bary, t0_bary_offset, epoch_distance,
 
     # Correct transit duration based on relative orbital velocity
     # of circular versus eccentric case
+    # Subtract (w_bary - 90) to match batman and PyTransit coordinate system
     if ecc_bary > 0:
         tdur /= 1 / sqrt(1 - ecc_bary ** 2) * (1 + ecc_bary * cos((w_bary - 90) / 180 * pi))
 
