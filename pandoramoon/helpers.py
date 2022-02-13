0d0de050938a4529a36790049e7af6c4938af513
Michael Hippke <hippke@users.noreply.github.com>
2022-02-13 15:36:43 +0100

    Add files via upload

--- pandoramoon/helpers.py
+++ pandoramoon/helpers.py
@@ -19,3 +19,9 @@ def ld_convert(q1, q2):
     u1 = 2 * np.sqrt(q1) * q2
     u2 = np.sqrt(q1) * (1 - 2 * q2)
     return u1, u2
+
+@jit(cache=True, nopython=True, fastmath=True, parallel=False)
+def ld_invert(u1, u2):
+    q1 = (u1 + u2)**2
+    q2 = u1 / (2 * (u1 + u2))
+    return q1, q2
